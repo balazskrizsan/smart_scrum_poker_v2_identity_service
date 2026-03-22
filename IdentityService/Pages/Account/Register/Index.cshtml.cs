4c9ff8d9772dc26b23eb270f7725193c77f0291c
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-03-22 21:38:36 +0100

    Registration form added

---
+++ IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,95 @@
+using Duende.IdentityServer;
+using Duende.IdentityServer.Events;
+using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Register;
+
+[SecurityHeaders]
+[AllowAnonymous]
+public class Index : PageModel
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly IIdentityServerInteractionService _interaction;
+    private readonly IEventService _events;
+
+    public ViewModel View { get; set; } = default!;
+        
+    [BindProperty]
+    public InputModel Input { get; set; } = default!;
+        
+    public Index(
+        IIdentityServerInteractionService interaction,
+        IEventService events,
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _interaction = interaction;
+        _events = events;
+    }
+
+    public async Task<IActionResult> OnGet(string? returnUrl)
+    {
+        Input = new InputModel
+        {
+            ReturnUrl = returnUrl
+        };
+
+        return Page();
+    }
+        
+    public async Task<IActionResult> OnPost()
+    {
+        // the user clicked the "cancel" button
+        if (Input.Button != "register")
+        {
+            return Redirect(Input.ReturnUrl ?? "~/");
+        }
+
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser
+            {
+                UserName = Input.Username,
+                Email = Input.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, Input.Password!);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                await _events.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.UserName, clientId: null));
+                Telemetry.Metrics.UserLogin(null, IdentityServerConstants.LocalIdentityProvider);
+
+                if (Url.IsLocalUrl(Input.ReturnUrl))
+                {
+                    return Redirect(Input.ReturnUrl);
+                }
+                else if (string.IsNullOrEmpty(Input.ReturnUrl))
+                {
+                    return Redirect("~/");
+                }
+                else
+                {
+                    // user might have clicked on a malicious link - should be logged
+                    throw new ArgumentException("invalid return URL");
+                }
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        // If we got this far, something failed, redisplay form
+        return Page();
+    }
+}
